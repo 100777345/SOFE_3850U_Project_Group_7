297a8059b31b744afea537d182b18b918fe16e13
100777345 <nagahiro.aoyama@ontariotechu.net>
2023-03-20 20:15:10 -0400

    v1 - functional

--- Part 4/udp_file_client.c
+++ Part 4/udp_file_client.c
@@ -16,6 +16,10 @@
 
 #define	MSG		"Any Message \n"
 
+struct pdu {
+	char type;
+	char data[100];
+};
 
 /*------------------------------------------------------------------------
  * main - UDP client for TIME service that prints the resulting time
@@ -26,22 +30,19 @@ main(int argc, char **argv)
 {
 	char	*host = "localhost";
 	int	port = 3000;
-	char	now[100];		/* 32-bit integer to hold time	*/ 
+	char	contents[100];		/* 32-bit integer to hold time	*/ 
 	struct hostent	*phe;	/* pointer to host information entry	*/
 	struct sockaddr_in sin;	/* an Internet endpoint address		*/
 	int	s, n, type;	/* socket descriptor and socket type	*/
+	FILE *fpt;
 
 	switch (argc) {
-	case 1:
-		break;
-	case 2:
-		host = argv[1];
 	case 3:
 		host = argv[1];
 		port = atoi(argv[2]);
 		break;
 	default:
-		fprintf(stderr, "usage: UDPtime [host [port]]\n");
+		fprintf(stderr, "usage: %s [host] [port]\n", argv[0]);
 		exit(1);
 	}
 
@@ -64,16 +65,71 @@ main(int argc, char **argv)
                                                                                 
     /* Connect the socket */
         if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-		fprintf(stderr, "Can't connect to %s %s \n", host, "Time");
+		fprintf(stderr, "Connection to %s %s \n", host, "failed.");
 
 
-	(void) write(s, MSG, strlen(MSG));
+	struct pdu rpdu;
+	struct pdu spdu;
+	int option;
 
-	/* Read the time */
+while (1){
 
-	n = read(s, (char *)&now, sizeof(now));
-	if (n < 0)
-		fprintf(stderr, "Read failed\n");
-	write(1, now, n);
+	// Main menu
+	fprintf(stdout, "Enter 1 to download a file, or 2 to quit.\n");
+	scanf("%d", &option);
+	if (option == 2)
+		exit(0);
+
+	// Take file name input
+	fprintf(stdout, "Enter the name of the file to download.\n");
+	spdu.type = 'C';
+	scanf("%s", spdu.data);
+
+	// Write to server
+	(void) write(s, &spdu, strlen(spdu.data)+1);
+
+	fprintf(stdout, "Packet sent. Type: \"%c\", Value: \"%s\".\n", spdu.type, spdu.data);
+
+	// Create file
+	fpt = fopen(spdu.data, "w");
+
+	// Recieve packets from server
+	do {
+	n = read(s, &rpdu, sizeof(rpdu.data)+1);
+
+	if (n < 0){
+		fprintf(stderr, "Failed to receive PDU from server.\n");
+		break;
+	}
+	
+	// Write to file if type is data
+	if (rpdu.type == 'D')
+		fputs(rpdu.data, fpt);
+
+	// Print received packet
+	fprintf(stdout, "Packet received. Type: \"%c\", Value: \"%s\".\n", rpdu.type, rpdu.data);
+
+	// Clear rpdu
+	memset(rpdu.data, 0, sizeof(rpdu.data));
+
+	} while (rpdu.type != 'E' && rpdu.type != 'F');
+	
+	// Display result
+	if (rpdu.type == 'E'){
+		fprintf(stderr, "Error during read.\n");
+		
+		// Delete created blank file if file was not found on server
+		fseek(fpt,0,SEEK_END);
+		int size = ftell(fpt);
+		if (size == 0)
+			remove(spdu.data);
+
+	} else if (rpdu.type == 'F')
+		fprintf(stdout, "File successfully downloaded.\n");
+
+	fclose(fpt);
+
+}
+
 	exit(0);
 }
--- Part 4/udp_file_server.c
+++ Part 4/udp_file_server.c
@@ -9,6 +9,10 @@
 #include <stdio.h>
 #include <time.h>
 
+struct pdu {
+	char type;
+	char data[100];
+};
 
 /*------------------------------------------------------------------------
  * main - Iterative UDP server for TIME service
@@ -18,19 +22,18 @@ int
 main(int argc, char *argv[])
 {
 	struct  sockaddr_in fsin;	/* the from address of a client	*/
-	char	buf[100];		/* "input" buffer; any size > 0	*/
 	char    *pts;
 	int	sock;			/* server socket		*/
-	time_t	now;			/* current time			*/
 	int	alen;			/* from-address length		*/
 	struct  sockaddr_in sin; /* an Internet endpoint address         */
-        int     s, type;        /* socket descriptor and socket type    */
+    int     s, type;        /* socket descriptor and socket type    */
 	int 	port=3000;
+	FILE *fpt;
                                                                                 
 
 	switch(argc){
-		case 1:
-			break;
+		/*case 1:
+			break;*/
 		case 2:
 			port = atoi(argv[1]);
 			break;
@@ -47,24 +50,61 @@ main(int argc, char *argv[])
     /* Allocate a socket */
         s = socket(AF_INET, SOCK_DGRAM, 0);
         if (s < 0)
-		fprintf(stderr, "can't creat socket\n");
+		fprintf(stderr, "Failed to create socket.\n");
                                                                                 
     /* Bind the socket */
         if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-		fprintf(stderr, "can't bind to %d port\n",port);
+		fprintf(stderr, "Can't bind to %d port\n",port);
         listen(s, 5);	
 	alen = sizeof(fsin);
 
+	struct pdu request;
+	struct pdu response;
+
 	while (1) {
 		
-		if (recvfrom(s, buf, sizeof(buf), 0,
-				(struct sockaddr *)&fsin, &alen) < 0)
+		// Initialize type
+		response.type = 'F';
+
+		// Recieves packet
+		if (recvfrom(s, &request, sizeof(request.data)+1, 0, (struct sockaddr *)&fsin, &alen) < 0)
+		{
 			fprintf(stderr, "recvfrom error\n");
+			response.type = 'E';
+		}
+		fprintf(stdout, "Packet received. Type: \"%c\", Value: \"%s\".\n", request.type, request.data);
 
-		(void) time(&now);
-        	pts = ctime(&now);
+		// Cleans file name for fopen
+		char temp[100];
+		snprintf(temp, sizeof(temp), "%s", request.data);
 
-		(void) sendto(s, pts, strlen(pts), 0,
-			(struct sockaddr *)&fsin, sizeof(fsin));
+		// Opens file
+		fpt = fopen(request.data, "r");	
+
+		if (fpt == NULL){
+			strcpy(response.data, "FILE NOT FOUND");
+			response.type = 'E';
+		}
+			
+		// Send error packet
+		if (response.type == 'E') {
+			(void) sendto(s, &response, strlen(response.data)+1, 0, (struct sockaddr *)&fsin, sizeof(fsin));
+			fprintf(stdout, "Packet sent. Type: \"%c\", Value: \"%s\".\n", response.type, response.data);
+		} else {
+			response.type = 'D';
+
+			// Send all data
+			while(fgets(response.data, 100, fpt) != NULL){
+				(void) sendto(s, &response, strlen(response.data)+1, 0, (struct sockaddr *)&fsin, sizeof(fsin));
+				fprintf(stdout, "Packet sent. Type: \"%c\", Value: \"%s\".\n", response.type, response.data);
+			}
+
+			// Signal end
+			response.type = 'F';
+			(void) sendto(s, &response, strlen(response.data+1), 0, (struct sockaddr *)&fsin, sizeof(fsin));
+			fprintf(stdout, "Packet sent. Type: \"%c\", Value: \"%s\".\n", response.type, response.data);
+
+		}
+
 	}
 }
